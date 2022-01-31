1a4e5a323ce43982b472e9503d2cad4ce1b9582e
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2022-01-31 20:04:25 +0530

    add Log.d

--- app/src/main/java/com/example/horizontalscrollbardemo/MainActivity.java
+++ app/src/main/java/com/example/horizontalscrollbardemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.horizontalscrollbardemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,13 +18,14 @@ public class MainActivity extends AppCompatActivity {
 
         button=findViewById(R.id.button);
         String name="Name : Rushikesh Narkhede\nAppName:- HorizontalscrollBar\nDate :- 31/01/2022\n";
-
+        Log.d("Rushi", "This button is a working properly");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
-
+                Log.d("Rushi", "This button is working properly");
+                //log is use to print message on console.
             }
         });
     }
